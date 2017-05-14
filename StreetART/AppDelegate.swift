ee4cc2279eb16b5ca57ee4d254e74223938982db
georgeallard <GeorgeAllard@Georges-MacBook-Pro.local>
2017-05-15 00:29:41 +0100

    Tab bar

--- StreetART/AppDelegate.swift
+++ StreetART/AppDelegate.swift
@@ -47,9 +47,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        UINavigationBar.appearance().barTintColor = UIColor.gray
+        
+        
         // Override point for customization after application launch.
         FIRApp.configure()
         return true
+        
+
+        
+
     }
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -72,6 +80,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+
 
 
 
