fc26083ded2e2248f22377db54c9c4005d4f08fc
georgeallard <GeorgeAllard@Georges-MacBook-Pro.local>
2017-05-15 20:04:01 +0100

    Profile Page

---
+++ StreetART/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  StreetART
+//
+//  Created by George Allard on 15/05/2017.
+//  Copyright © 2017 George Allard. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
